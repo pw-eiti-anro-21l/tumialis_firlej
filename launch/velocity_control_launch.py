b6b8aa3bcc30e724e06ca62bdf7e5154419ee69a
madvx <80012963+madvx@users.noreply.github.com>
2021-03-20 19:14:46 +0100

    Upload pliku roslaunch

---
+++ launch/velocity_control_launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            namespace='',
+            executable='turtlesim_node',
+            name='turtle1'
+        ),
+        Node(
+            package='zadanie1',
+            prefix='gnome-terminal --',
+            namespace='',
+            executable='talker',
+            name='velocityNode'
+        )
+    ])
